582cefe5d89edfe677f6e6a1e6a3d91d89213cb1
Curso <Curso@Formacion-TOSH1.ipartekaula.com>
2017-03-16 13:22:03 +0100

    Swagger configuracion REST

--- gestion-docente-spring/src/main/java/com/ipartek/formacion/api/restfulservers/alumno/configuration/AlumnoRestInitializer.java
+++ gestion-docente-spring/src/main/java/com/ipartek/formacion/api/restfulservers/alumno/configuration/AlumnoRestInitializer.java
@@ -2,30 +2,29 @@ package com.ipartek.formacion.api.restfulservers.alumno.configuration;
 
 import javax.servlet.Filter;
 
-import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 import com.ipartek.formacion.api.restfulservers.CORSFilter;
 
 // Para hacer esta clase; En superclass -->  browse.. --> y buscar : AbstractAnnotationConfigDispatcherServletInitializer para que extienda de ella.
 
-public class AlumnoRestInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+public class AlumnoRestInitializer  {
 
-	@Override
+
 	protected Class<?>[] getRootConfigClasses() {
 		return new Class[]{AlumnosRestControllerConfiguration.class};
 	}
 
-	@Override
+
 	protected Class<?>[] getServletConfigClasses() {
 		return null;
 	}
 
-	@Override
+
 	protected String[] getServletMappings() {
 		return new String[]{"/"};
 	}
 
-	@Override
+
 	protected Filter[] getServletFilters() {
 		Filter[] filter = {new CORSFilter()};
 		return filter;
